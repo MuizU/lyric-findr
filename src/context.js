6f3cd247b9be29c07b0f57031661073965e70473
MuizU <muizuvais99@gmail.com>
2021-05-26 00:41:29 +0530

    added custom request to get from Shazam

--- src/context.js
+++ src/context.js
@@ -1,13 +1,27 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 
 const Context = React.createContext()
 
 export class Provider extends Component {
   state = {
-    track_list:{ hits:[ 
-      {track:{layout:"5",type:"MUSIC",title:"Kiss The Rain"}}
-    ],artists:{hits:[] }},
+    track_list:{},
     heading:'Top 10 Tracks'
+  }
+
+  async componentDidMount(){
+  const options = {
+    method: 'GET',
+    url: 'https://shazam.p.rapidapi.com/charts/track',
+    params: {locale: 'en-US', pageSize: '10', startFrom: '0'},
+    headers: {
+      'x-rapidapi-key': process.env.REACT_APP_SHZM_KEY,
+      'x-rapidapi-host': 'shazam.p.rapidapi.com'
+    }
+  };
+  const res = await axios.request(options)
+  this.setState({track_list: res.data.tracks})
+    
   }
   render() {
     return (
